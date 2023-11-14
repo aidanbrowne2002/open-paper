f26d784f35bb463316cf1f8c7ec8096463c1698e
aidanbrowne2002 <62239752+aidanbrowne2002@users.noreply.github.com>
2023-11-14 14:23:05 +0000

    initial api

--- test_bitmap.py
+++ test_bitmap.py
@@ -4,23 +4,26 @@ def generate_checkerboard(width, height):
     for y in range(height):
         row = []
         for x in range(width):
-            if (x // 8) % 2 == (y // 8) % 2:
-                row.append("0XFF")
-            else:
-                row.append("0X00")
-        bitmap.append(''.join(row))
+            # XOR of x and y to create a checkerboard pattern
+            pixel_value = 0xFF if (x // 8) % 2 == (y // 8) % 2 else 0x00
+            row.append(pixel_value)
+
+        # Convert the row to hexadecimal format
+        hex_row = [f'0x{format(byte, "02X")}' for byte in row]
+        row_str = ','.join(hex_row)
+        bitmap.append(row_str)
 
     return bitmap
 
+def display_bitmap(bitmap):
+    for row in bitmap:
+        print(row)
+    return bitmap
 
-def print_bitmap(bitmap):
-    print('\n'.join(bitmap))
-
-
+# Generate and display the checkerboard bitmap for a 250x122 display
 def createImage():
     width = 250
     height = 122
-
-    bitmap = generate_checkerboard(width, height)
-    print_bitmap(bitmap)
+    checkerboard_bitmap = generate_checkerboard(width, height)
+    bitmap = display_bitmap(checkerboard_bitmap)
     return bitmap
