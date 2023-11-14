f61b3d2578af756f7b4b54f7f4dc0acf8c2b04a7
aidanbrowne2002 <62239752+aidanbrowne2002@users.noreply.github.com>
2023-11-14 14:29:41 +0000

    initial api

--- test_bitmap.py
+++ test_bitmap.py
@@ -1,36 +1,37 @@
-def generate_checkerboard(width, height):
-    bitmap = []
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
-    for y in range(height):
-        row = []
-        for x in range(width):
-            # XOR of x and y to create a checkerboard pattern
-            pixel_value = 0xFF if (x // 8) % 2 == (y // 8) % 2 else 0x00
-            row.append(pixel_value)
+def create_blank_image(width, height):
+    return Image.new("L", (width, height), "white")  # "L" mode for monochrome images
 
-        # Convert the row to hexadecimal format
-        hex_row = [f'0x{format(byte, "02X")}' for byte in row]
-        row_str = ','.join(hex_row)
-        bitmap.append(row_str)
+def image_to_binary_bitmap(image):
+    width, height = image.size
+    pixels = list(image.getdata())
 
-    # Join all rows to create a single string with 250x16 bytes
-    full_bitmap = ','.join(bitmap)
+    binary_values = ['1' if pixel < 128 else '0' for pixel in pixels]
+    binary_string = ''.join(binary_values)
 
-    return full_bitmap
+    # Group the binary string into rows of 8 pixels
+    grouped_binary = [binary_string[i:i + width] for i in range(0, len(binary_string), width)]
 
+    # Ensure each row is represented by 16 bytes, adding 8 zeros between rows
+    grouped_binary = [row + '00000000' for row in grouped_binary]
 
-def display_bitmap(bitmap):
-    return bitmap
+    # Convert each row to hex
+    hex_values = [f'0X{int(group[::-1], 2):02X}' for group in grouped_binary[::-1]]
 
+    hex_bitmap = ','.join(hex_val for hex_val in hex_values)
+    return hex_bitmap
 
-# Generate and display the checkerboard bitmap for a 250x122 display
 def createImage():
-    width = 250
-    height = 122
-    checkerboard_bitmap = generate_checkerboard(width, height)
-    full_bitmap = display_bitmap(checkerboard_bitmap)
-    return full_bitmap
+    width, height = 122, 250
+    i = create_blank_image(width, height)
 
+    Im = ImageDraw.Draw(i)
+    mf = ImageFont.truetype('/home/aidanbrowne2002/open-paper/fonts/BlockStockRegular-A71p.ttf', 25)
+    Im.text((15, 15), "L", 0, font=mf)  # Use 0 for monochrome (black) color
 
-# Call the function to create and display the image
-createImage()
+    hex_bitmap = image_to_binary_bitmap(i)
+    print(hex_bitmap)
+    return hex_bitmap
