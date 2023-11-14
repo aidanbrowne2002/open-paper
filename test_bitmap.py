d0e2cf40ac54c71d6b6e33c8ebad89570d2347b1
aidanbrowne2002 <62239752+aidanbrowne2002@users.noreply.github.com>
2023-11-14 12:24:20 +0000

    initial api

--- test_bitmap.py
+++ test_bitmap.py
@@ -10,7 +10,7 @@ def image_to_binary_bitmap(image):
     binary_values = ['1' if pixel < 128 else '0' for pixel in pixels]
     binary_string = ''.join(binary_values)
     grouped_binary = [binary_string[i:i+8] for i in range(0, len(binary_string), 8)]
-    hex_values = [','.join([f'0X{int(pixel, 2):02X}' for pixel in group]) for group in grouped_binary]
+    hex_values = [f'0X{int(group, 2):02X}' for group in grouped_binary]
     hex_bitmap = ','.join(hex_val for hex_val in hex_values)
     return hex_bitmap
 
