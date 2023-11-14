d4926e790aad397d90b7211659f88a9c755014ba
aidanbrowne2002 <62239752+aidanbrowne2002@users.noreply.github.com>
2023-11-14 13:15:30 +0000

    initial api

--- test_bitmap.py
+++ test_bitmap.py
@@ -2,26 +2,39 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+
 def create_blank_image(width, height):
     return Image.new("L", (width, height), "white")  # "L" mode for monochrome images
 
+
 def image_to_binary_bitmap(image, width, height):
+    width, height = image.size
     pixels = list(image.getdata())
+
     binary_values = ['1' if pixel < 128 else '0' for pixel in pixels]
     binary_string = ''.join(binary_values)
-    grouped_binary = [binary_string[i:i+width] for i in range(0, len(binary_string), width)]
-    hex_values = [f'0X{int(group[::-1], 2):02X}' for group in grouped_binary[::-1]]
+
+    # Group the binary string into rows of bytes
+    grouped_binary = [binary_string[i:i + (width // 8) * 8] for i in range(0, len(binary_string), (width // 8) * 8)]
+
+    # Reverse the order of rows
+    grouped_binary = grouped_binary[::-1]
+
+    # Convert each row to hex
+    hex_values = [f'0X{int(group[::-1], 2):02X}' for group in grouped_binary]
+
     hex_bitmap = ','.join(hex_val for hex_val in hex_values)
     return hex_bitmap
+
 
 def createImage():
     width, height = 122, 250
     i = create_blank_image(width, height)
 
-    Im = ImageDraw.Draw(i)
+    Im = ImageDraw.Draw(i, width, height)
     mf = ImageFont.truetype('/home/aidanbrowne2002/open-paper/fonts/ShortBaby-Mg2w.ttf', 25)
     Im.text((15, 15), "0", 0, font=mf)  # Use 0 for monochrome (black) color
 
-    hex_bitmap = image_to_binary_bitmap(i, width, height)
+    hex_bitmap = image_to_binary_bitmap(i)
     print(hex_bitmap)
     return hex_bitmap
