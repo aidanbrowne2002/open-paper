c30b0a1907481d8b6eda0a55b1b0e95e054ef53b
aidanbrowne2002 <62239752+aidanbrowne2002@users.noreply.github.com>
2023-11-14 14:25:34 +0000

    initial api

--- test_bitmap.py
+++ test_bitmap.py
@@ -8,9 +8,11 @@ def generate_checkerboard(width, height):
             pixel_value = 0xFF if (x // 8) % 2 == (y // 8) % 2 else 0x00
             row.append(pixel_value)
 
-        # Convert the row to hexadecimal format
+        # Convert the row to 16 bytes (representing 122 pixels)
         hex_row = [f'0x{format(byte, "02X")}' for byte in row]
         row_str = ','.join(hex_row)
+        # Duplicate the row to represent 16 bytes
+        row_str *= 16
         bitmap.append(row_str)
 
     return bitmap
@@ -27,3 +29,6 @@ def createImage():
     checkerboard_bitmap = generate_checkerboard(width, height)
     bitmap = display_bitmap(checkerboard_bitmap)
     return bitmap
+
+# Call the function to create and display the image
+createImage()
