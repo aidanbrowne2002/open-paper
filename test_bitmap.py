98e5f94191b7d763f9568b3bb07529adfdebf9a8
aidanbrowne2002 <62239752+aidanbrowne2002@users.noreply.github.com>
2023-11-14 14:46:30 +0000

    initial api

--- test_bitmap.py
+++ test_bitmap.py
@@ -2,36 +2,47 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+
+def binary_to_hex(arr):
+    hex_array = []
+
+    for row in arr:
+        binary_string = ''.join(map(str, row))
+        decimal_value = int(binary_string, 2)
+        hex_value = hex(decimal_value)
+        hex_array.append(hex_value)
+
+    return hex_array
 def create_blank_image(width, height):
     return Image.new("L", (width, height), "white")  # "L" mode for monochrome images
 
 def image_to_binary_bitmap(image):
     width, height = image.size
     pixels = list(image.getdata())
+    for i in range(len(pixels)):
+        if pixels[i] < 128:
+            pixels[i] = 0
+        else:
+            pixels[i] = 1
 
-    binary_values = ['1' if pixel < 128 else '0' for pixel in pixels]
-    binary_string = ''.join(binary_values)
+    for i in range(0, len(pixels), 250):
+        chunk = pixels[i:i + 250]
+        #print(chunk)
 
-    # Group the binary string into rows of 8 pixels
-    grouped_binary = [binary_string[i:i + width] for i in range(0, len(binary_string), width)]
+    pixelarray = []
+    for i in range(0, len(pixels), 8):
+        pixelarray.append(pixels[i:i + 8])
+    #print(pixelarray)
 
-    # Ensure each row is represented by 16 bytes, adding 8 zeros between rows
-    grouped_binary = [row + '00000000' for row in grouped_binary]
-
-    # Convert each row to hex
-    hex_values = [f'0X{int(group[::-1], 2):02X}' for group in grouped_binary[::-1]]
-
-    hex_bitmap = ','.join(hex_val for hex_val in hex_values)
-    return hex_bitmap
-
+    hexarray = binary_to_hex(pixelarray)
+    return hexarray
 def createImage():
-    width, height = 122, 250
+    width, height = 250, 122
     i = create_blank_image(width, height)
 
     Im = ImageDraw.Draw(i)
-    mf = ImageFont.truetype('/home/aidanbrowne2002/open-paper/fonts/BlockStockRegular-A71p.ttf', 25)
-    Im.text((15, 15), "L", 0, font=mf)  # Use 0 for monochrome (black) color
+    mf = ImageFont.truetype('fonts/BlockStockRegular-A71p.ttf', 25)
+    Im.text((15, 15), "Lov", 0, font=mf)  # Use 0 for monochrome (black) color
 
     hex_bitmap = image_to_binary_bitmap(i)
-    print(hex_bitmap)
     return hex_bitmap
