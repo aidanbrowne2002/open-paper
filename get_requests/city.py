95a4a8e70d20bf055abac2b13d1e2012aa64ae37
aidanbrowne2002 <62239752+aidanbrowne2002@users.noreply.github.com>
2023-11-23 11:25:36 +0000

    initial api

---
+++ get_requests/city.py
@@ -0,0 +1,16 @@
+def getcity(ip):
+    response = requests.get(f"https://geolocation-db.com/jsonp/{ip}")
+    #response = requests.get(url)
+    data = response.text
+
+    # Extracting the JSON data from the callback function
+    json_data = data.split("callback(")[1].rstrip(")")
+
+    # Now you can parse the JSON data
+    parsed_data = json.loads(json_data)
+
+    # Access the city information
+    city = parsed_data["city"]
+
+    return city
+
