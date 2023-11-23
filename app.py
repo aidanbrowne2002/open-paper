e09f80ccc0d924a56516df484b86536c3c31a15b
aidanbrowne2002 <62239752+aidanbrowne2002@users.noreply.github.com>
2023-11-23 09:58:11 +0000

    initial api

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello_world():  # put application's code here
 @app.route('/api/initial/')
 def initalAPI():
     device = request.args.get('device')
-    return jsonify({'api': 'aidanbrowne2002.eu.pythonanywhere.com/api/weather/','check-in': '10'}), 200
+    return jsonify({'api': 'http://aidanbrowne2002.eu.pythonanywhere.com/api/weather/','check-in': '10'}), 200
 
 
 @app.route('/api/weather/')
